dd5a02dc0e01112293815118a359df20b3d05ad9
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-04-29 22:24:25 -0300

    Updating total price Working (but I found a new issue... the cancel button of itens add isn't working)

--- js/application.js
+++ js/application.js
@@ -15,13 +15,22 @@ class shoppingCardItens {
                 <td>${this.unityPrice}</td>
                 <td><span>QTY</span><input class="input-qty" type='number' value='${this.qty}'></input></td>
                 <td><button class="delete-item">Cancel</button></td>
-                <td>${this.totalItem()}</td>
+                <td class='total-item'>${this.totalItem()}</td>
                 </tr>`
     }
 
     includeToMainTable() {
         $('#main-table').append( this.lineTable() );
     }
+}
+
+let updatingTotalValue = function() {
+    let sum = 0;
+    $('.total-item').each( function(){
+        sum += Number( $(this).text() );
+    })
+
+    $('#total-price').text(sum);
 }
 
 let itensDefault = [new shoppingCardItens("Salmon" , 60 , 2),
@@ -33,7 +42,9 @@ let itensDefault = [new shoppingCardItens("Salmon" , 60 , 2),
 let loadDefaultData = function() {
     itensDefault.forEach( function(itemDefault) { 
        itemDefault.includeToMainTable( itemDefault.lineTable() );
-    }) 
+    })
+
+    updatingTotalValue();
 }
 
 let includeItem = function (event) {
@@ -43,6 +54,7 @@ let includeItem = function (event) {
     let newQty = $(this).children('[name = qty]').val();
     let myNewItem = new shoppingCardItens(newName, newUnitaryPice, newQty);
     myNewItem.includeToMainTable( myNewItem.lineTable() );
+    updatingTotalValue();
 }
 
 var timeOut;
@@ -61,20 +73,20 @@ let updateQty = function() {
         let unityPrice = currentRow.children('td').html();           
         let modifyItem = new shoppingCardItens(name, unityPrice, qty);    
         currentRow.replaceWith( modifyItem.lineTable() );
+        updatingTotalValue();
     } , 500);   
 }
 
 let deleteItem = function () {
     $(this).closest('tr').remove();
+    updatingTotalValue();
 }
 
 //------------------MAIN-----------------------
-
-
 $(document).ready( function(){
     loadDefaultData();
     $('#add-item').on('submit' , includeItem);
-    $('.input-qty').on('input' , updateQty);
+    $('.input-qty').on('input' , updateQty); //Something is wrong, when I tried to change a second time the same element this line doesn't work
     $('.delete-item').on('click' , deleteItem);
 });
 
